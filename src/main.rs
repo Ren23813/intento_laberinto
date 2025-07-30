a94620a4483320508a39945b0d6777fb62de1e55
Renato Rojas <roj23813@uvg.edu.gt>
2025-07-30 12:01:07 -0600

    fallo en texturas paredes

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod framebuffer;
 mod maze;
 mod player;
 mod caster;
+mod textures;
+mod enemy;
 
 use raylib::prelude::*;
 use std::thread;
@@ -13,6 +15,8 @@ use framebuffer::Framebuffer;
 use maze::{Maze,load_maze};
 use player::Player;
 use std::f32::consts::PI;
+use textures::TextureManager;
+use enemy::Enemy;
 
 use crate::{caster::cast_ray, player::process_events};
 
@@ -55,12 +59,126 @@ pub fn render_maze(
     //draw player
     framebuffer.set_current_color(Color::WHITE);
     framebuffer.set_pixel(player.pos.x as i32, player.pos.y as i32);
-    cast_ray(framebuffer, maze, player, block_size);
+
+    //dibujar n rayos
+    let num_rays = 5; //framebuffer.width;
+    for i in 0..num_rays {
+        let current_ray = i as f32 / num_rays as f32;
+        let a = player.a -(player.fov/2.0)+(player.fov*current_ray);
+        cast_ray(framebuffer, maze, player, a,block_size,true);
+    }
+}
+
+pub fn render_world(framebuffer:&mut Framebuffer, player: &Player,maze:&Maze, texture_cache: &TextureManager){
+    // let maze = load_maze("./maze.txt");
+    let block_size = 100;
+    let num_rays = framebuffer.width;
+
+    let hw = framebuffer.width as f32/2.0;
+    let hh = framebuffer.height as f32/2.0;
+
+    framebuffer.set_current_color(Color::WHITE);
+    
+    for i in 0..num_rays{
+        let current_ray = i as f32 / num_rays as f32;
+        let a = player.a - (player.fov/2.0) + (player.fov*current_ray);
+        let intersect = cast_ray(framebuffer, &maze, &player, a, block_size, false);
+        let d = intersect.distance;
+        let c = intersect.impact;
+
+        let angle_diff = a-player.a;
+        let mut distance_to_wall = intersect.distance*angle_diff.cos();
+        if distance_to_wall < 0.1 {
+            distance_to_wall = 0.2;
+            // continue; 
+        }
+        let stake_height = (hh/distance_to_wall) *70.0;
+        
+
+        let stake_top = (hh - (stake_height / 2.0)).max(0.0) as usize;
+        let stake_bottom = (hh + (stake_height / 2.0)).min(framebuffer.height as f32) as usize;
+        
+
+        for y in stake_top..stake_bottom{
+            let tx = intersect.tx;
+            let ty = (y as f32 -stake_top as f32 ) / (stake_bottom as f32  - stake_top as f32 ) *1216.0;
+
+            let color = texture_cache.get_pixel_color(c,tx as u32, ty as u32);
+            framebuffer.set_current_color(color);
+            // match c {
+            //     '|' => framebuffer.set_current_color(color),
+            //     _ => framebuffer.set_current_color(Color::new(255, 255, 255, 255)), 
+            // }
+            framebuffer.set_pixel(i, y as i32);
+
+        }
+
+    }
+}
+
+const TRANSPARENT_COLOR: Color = Color::new(152, 0, 136, 255);
+fn draw_sprite(
+    framebuffer: &mut Framebuffer,
+    player: &Player,
+    enemy: &Enemy, //cambiar
+    texture_manager: &TextureManager
+) {
+    let sprite_a = (enemy.pos.y - player.pos.y).atan2(enemy.pos.x - player.pos.x);
+    let mut angle_diff = sprite_a - player.a;
+    while angle_diff > PI {
+        angle_diff -= 2.0 * PI;
+    }
+    while angle_diff < -PI {
+        angle_diff += 2.0 * PI;
+    }
+
+    if angle_diff.abs() > player.fov / 2.5 {
+        return;
+    }
+
+    let sprite_d: f32 = ((player.pos.x - enemy.pos.x).powi(2) + (player.pos.y - enemy.pos.y).powi(2)).sqrt();
+
+    // near plane           far plane
+    if sprite_d < 50.0 || sprite_d > 1000.0 {
+        return;
+    }
+
+    let screen_height = framebuffer.height as f32;
+    let screen_width = framebuffer.width as f32;
+
+    let sprite_size = (screen_height / sprite_d) * 70.0;
+    let screen_x = ((angle_diff / player.fov) + 0.5) * screen_width;
+
+    let start_x = (screen_x - sprite_size / 2.0).max(0.0) as usize;
+    let start_y = (screen_height / 2.0 - sprite_size / 2.0).max(0.0) as usize;
+    let sprite_size_usize: usize = sprite_size as usize;
+    let end_x = (start_x + sprite_size_usize).min(framebuffer.width as usize);
+    let end_y = (start_y + sprite_size_usize).min(framebuffer.height as usize);
+
+    for x in start_x..end_x {
+        for y in start_y..end_y {
+            let tx = ((x - start_x) * 1216 / sprite_size_usize) as u32; //128
+            let ty = ((y - start_y) * 1216 / sprite_size_usize) as u32; //128
+
+            let color = texture_manager.get_pixel_color(enemy.texture_key, tx, ty);
+            
+            if color != TRANSPARENT_COLOR {
+                framebuffer.set_current_color(color);
+                framebuffer.set_pixel(x as i32, y as i32);
+            }
+        }
+    }
 }
 
 
-
-
+fn render_enemies(framebuffer:&mut Framebuffer, player: &Player, texture_cache: &TextureManager){
+    let enemies = vec![
+        Enemy::new(250.0, 250.0, 'e')
+    ];
+    for enemy in enemies{
+        draw_sprite(framebuffer, &player, &enemy, texture_cache);
+    }
+}
 
 fn main() {
     let window_width = 1300;
@@ -79,19 +197,30 @@ fn main() {
 
     // Load the maze once before the loop
     let maze = load_maze("./maze.txt");
-    let mut player = Player{pos:(Vector2::new(180.0,180.0)), a: PI/3.0 };
+    let mut player = Player{pos:(Vector2::new(180.0,180.0)), a: PI/3.0, fov: PI/2.0 };
+    let texture_cache = TextureManager::new(&mut window, &raylib_thread);
 
     while !window.window_should_close() {
-
-        process_events(&window, &mut player);
+        framebuffer.clear();
+        process_events(&window, &mut player, &maze);
         // 1. clear framebuffer
+        let mut mode = "3D";
+
+        if window.is_key_down(KeyboardKey::KEY_M) {
+            mode = if mode =="2D" {"3D"} else {"2D"};
+        }
         framebuffer.clear();
 
-        // 2. draw the maze, passing the maze and block size
-        render_maze(&mut framebuffer, &maze, block_size,&player);
+        if mode == "2D"{
+            render_maze(&mut framebuffer, &maze, block_size,&player);
+        }
+        else {
+            render_world(&mut framebuffer,&player,&maze,&texture_cache);
+            render_enemies(&mut framebuffer,&player,&texture_cache);
+        }
 
-        // 3. swap buffers
-        framebuffer.swap_buffers(&mut window, &raylib_thread);
+        
+            framebuffer.swap_buffers(&mut window, &raylib_thread);
 
         thread::sleep(Duration::from_millis(16));
     }
